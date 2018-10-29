c61f1d2983e066498ad03459d7db972c64dae40b
hxllhhy <979641269@qq.com>
2018-10-30 01:09:14 +0800

    .

---
+++ o2o/src/main/java/com/yaya/o2o/dao/ShopCategoryDao.java
@@ -0,0 +1,10 @@
+package com.yaya.o2o.dao;
+
+import com.yaya.o2o.entity.ShopCategory;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ShopCategoryDao {
+    List<ShopCategory> queryShopCategory(@Param("shopCategoryCondition") ShopCategory shopCategory);
+}
---
+++ o2o/src/test/java/com/yaya/o2o/dao/ShopCategoryTest.java
@@ -0,0 +1,22 @@
+package com.yaya.o2o.dao;
+
+import com.yaya.o2o.BaseTest;
+import com.yaya.o2o.entity.ShopCategory;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShopCategoryTest extends BaseTest {
+    @Autowired
+    private ShopCategoryDao shopCategoryDao;
+
+    @Test
+    public void testQueryShopCategory() {
+        List<ShopCategory> shopCategoryList = shopCategoryDao.queryShopCategory(new ShopCategory());
+        assertEquals(1, shopCategoryList.size());
+    }
+
+}
