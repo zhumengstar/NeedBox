75c8ad244c82a48a3485090c19ee3ecc49256adf
hxllhhy <979641269@qq.com>
2018-11-06 23:03:40 +0800

    getQueryString

--- o2o/src/main/webapp/resources/js/common/common.js
+++ o2o/src/main/webapp/resources/js/common/common.js
@@ -8,7 +8,7 @@ function changeVerifyCode(img) {
 function getQueryString(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg);
-    if(r != null) {
+    if (r != null) {
         return decodeURIComponent(r[2]);
     }
     return '';
