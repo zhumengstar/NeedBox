b2ea3e61e8cbbc4b87b9194734fe47fa23811f92
hxllhhy <979641269@qq.com>
2018-11-20 02:23:29 +0800

    首页后台

---
+++ o2o/src/main/java/com/yaya/o2o/dao/HeadLineDao.java
@@ -0,0 +1,16 @@
+package com.yaya.o2o.dao;
+
+import com.yaya.o2o.entity.HeadLine;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface HeadLineDao {
+
+    //根据传入的查询条件(头条名查询头条)
+    List<HeadLine> queryHeadLine(@Param("headLineCondition")HeadLine headLineCondition);
+
+    //根据头条id返回唯一的头条信息
+
+
+}
---
+++ o2o/src/test/java/com/yaya/o2o/dao/HeadLineDaoTest.java
@@ -0,0 +1,22 @@
+package com.yaya.o2o.dao;
+
+import com.yaya.o2o.BaseTest;
+import com.yaya.o2o.entity.HeadLine;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class HeadLineDaoTest extends BaseTest {
+    @Autowired
+    private HeadLineDao headLineDao;
+
+    @Test
+    public void testQueryArea() {
+        List<HeadLine> headLineList = headLineDao.queryHeadLine(new HeadLine());
+        assertEquals(4, headLineList.size());
+    }
+
+}
